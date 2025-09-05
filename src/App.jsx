0f746569ee30e3a049cdc85ae9ff235201d16fd5
k-gardn <rladnjs15@naver.com>
2025-09-05 15:19:43 +0900

    env추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:movieId" element={<Detail />} />
